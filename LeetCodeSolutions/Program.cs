62fe48542cfb8b183d2ed92eff819f7a47ceb507
Lucas Souza <souzacontato18@gmail.com>
2023-01-16 15:30:14 -0300

    Adicionar arquivos de projeto.

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using LeetCodeSolutions.Solutions;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        SolutionRomanNumbers solution = new SolutionRomanNumbers();
+        int number = solution.RomanToInt("LVIII"); 
+        Console.WriteLine(number);
+    }
+}
---
+++ LeetCodeSolutions/Solutions/SolutionRomanNumbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Solutions
+{
+    /*
+     https://leetcode.com/problems/roman-to-integer/
+     
+     */
+
+    public class SolutionRomanNumbers
+    {   
+
+        public int RomanToInt(string s)
+        {
+            char[] numbers = s.ToCharArray();
+            int finalNumber = 0;
+
+            for(int i = 0; i < numbers.Length;i++)
+            {
+                int digit = ConvertRomanToArabic(numbers[i]);
+                finalNumber += digit;
+            }
+
+            return finalNumber;
+        }
+
+
+
+        private int RomanNumeralsSubtractions(char romanNumeral)
+        {
+            return 0;
+        }
+
+
+        private int ConvertRomanToArabic(char romanNumeral)
+        {
+            switch (romanNumeral)
+            {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
